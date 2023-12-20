a95801d8e89984871cc7a410d9f20c5866405af7
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2023-12-20 14:04:57 -0500

    Creados los componentes Home y Tollbar para la página principal de la aplicación.

---
+++ componentes/toolbar/Toolbar.jsx
@@ -0,0 +1,14 @@
+import './Toolbar.css' 
+ 
+ const Toolbar = () => {
+   return (
+    <div className="toolbar">
+        <div className="button-container">
+            <button className="login-button">Iniciar Sesión</button>
+            <button className="register-button">Registrarse</button>
+        </div>
+    </div>
+   )
+ }
+
+ export default Toolbar
