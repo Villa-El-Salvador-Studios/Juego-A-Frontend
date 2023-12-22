325245f5f4e56bc0f043bf56bde6e574a9f67feb
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2023-12-22 14:25:34 -0500

    Modificado la página de registro para que se use el service de jugador para poder crear un nuevo Jugador usando la información ingresada por los inputs.

--- componentes/llenar-informacion/Llenar-informacion.jsx
+++ componentes/llenar-informacion/Llenar-informacion.jsx
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
 import './Llenar-informacion.css'
 
-const LlenarInformacion = ({textoH1, textoInput}) => {
+const LlenarInformacion = ({textoH1, textoInput, onInputChange}) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setInputValue(value);
+    
+        // Llama a la función de devolución de llamada para pasar la información al componente principal
+        onInputChange(textoInput, value);
+    };
+
     return (
         <div className='llenar-informacion'>
             <h1>{textoH1}</h1>
-            <input className="custom-input" type="text" placeholder={textoInput} />
+            <input className="custom-input" type="text" placeholder={textoInput} value={inputValue} onChange={handleChange} />
         </div>
     )
 }
--- paginas/registro/Registro.jsx
+++ paginas/registro/Registro.jsx
@@ -1,35 +1,69 @@
+import React, { useState } from 'react';
 import Toolbar from "../../componentes/toolbar/Toolbar"
 import LlenarInformacion from "../../componentes/llenar-informacion/Llenar-informacion"
+import JugadorService from "../../services/jugador-service"
 import "./Registro.css"
 
-const botones = {
-    icono_home: {
-        texto: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjEgMTN2MTBoLTZ2LTZoLTZ2NmgtNnYtMTBoLTNsMTItMTIgMTIgMTJoLTN6bS0xLTUuOTA3di01LjA5M2gtM3YyLjA5M2wzIDN6Ii8+PC9zdmc+",
-        ruta: "/home"
-    },
-    boton1: {
-        texto: "Iniciar Sesión",
-        ruta: "/inicio-sesion"
-    },
-    boton2: {
-        texto: "Registrarse",
-        ruta: "/registro"
-    }
-}
-
 const Registro = () => {
-     return (
-        <div className="espacio-registro">
-            <Toolbar botones={botones}></Toolbar>
-            <div className="centrado">
-                <LlenarInformacion textoH1="Usuario" textoInput="Usuario"/>
-                <LlenarInformacion textoH1="Contraseña" textoInput="Contraseña"/>
-                <LlenarInformacion textoH1="Repetir contraseña" textoInput="Repetir contraseña"/>
-                <br></br>
-                <p>Ingrese solo carácteres en minúscula.</p>
-            </div>
+    const [informacion, setInformacion] = useState({});
+
+    const onInputChange = (campo, valor) => {
+        setInformacion({
+          ...informacion,
+          [campo]: valor
+        });
+    };
+
+    const botones = {
+        icono_home: {
+            texto: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjEgMTN2MTBoLTZ2LTZoLTZ2NmgtNnYtMTBoLTNsMTItMTIgMTIgMTJoLTN6bS0xLTUuOTA3di01LjA5M2gtM3YyLjA5M2wzIDN6Ii8+PC9zdmc+",
+            ruta: "/home"
+        },
+        boton1: {
+            texto: "Iniciar Sesión",
+            ruta: "/inicio-sesion"
+        },
+        boton2: {
+            texto: "Registrarse",
+            ruta: "/registro"
+        }
+    }
+
+    const enviarInformacion = () => {
+        const data = {
+            "usuario": informacion.Usuario,
+            "contrasenia": informacion.Contraseña,
+            "fotoPerfil": "string",
+            "mundoMaximo": 0
+        }
+
+        JugadorService.Create(data)
+        .then(response => {
+        // Maneja la respuesta del servidor según tus necesidades
+        console.log('Respuesta del servidor:', response.data);
+        })
+        .catch(error => {
+        // Maneja los errores según tus necesidades
+        console.error('Error al enviar datos al servidor:', error);
+        });
+
+
+        console.log("enviarInformacion", informacion)
+    }
+
+    return (
+    <div className="espacio-registro">
+        <Toolbar botones={botones}></Toolbar>
+        <div className="centrado">
+            <LlenarInformacion textoH1="Usuario" textoInput="Usuario" onInputChange={onInputChange}/>
+            <LlenarInformacion textoH1="Contraseña" textoInput="Contraseña" onInputChange={onInputChange}/>
+            <LlenarInformacion textoH1="Repetir contraseña" textoInput="Repetir contraseña" onInputChange={onInputChange}/>
+            <br></br>
+            <button className="boton-registro" onClick={enviarInformacion}>Crear cuenta</button>
+            <p className="instrucciones">Ingrese solo carácteres en minúscula.</p>
         </div>
-     )
+    </div>
+    )
 }
 
 export default Registro
