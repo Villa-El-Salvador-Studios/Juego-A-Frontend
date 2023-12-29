e254cd9587552601b42d9fef7ee400c84b5639d9
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2023-12-29 17:33:02 -0500

    Creado el componente para el botón que servirá para seleccionar un nivel en el selector de niveles.

---
+++ componentes/boton-seleccionar-nivel/BotonSeleccionarNivel.jsx
@@ -0,0 +1,11 @@
+import './BotonSeleccionarNivel.css'
+
+const BotonSeleccionarNivel = () => {
+    return (
+        <div className="boton-seleccionar-nivel">
+            <button>Seleccionar nivel</button>
+        </div>
+    )    
+}
+
+export default BotonSeleccionarNivel
