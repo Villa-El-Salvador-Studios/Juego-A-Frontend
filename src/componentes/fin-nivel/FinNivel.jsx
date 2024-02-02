3d04597259eb7b3f9197b4df0fee047dc1aba2e6
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-02-02 13:30:33 -0500

    Mejorados los estilos del componente FinNivel

--- src/componentes/fin-nivel/FinNivel.jsx
+++ src/componentes/fin-nivel/FinNivel.jsx
@@ -1,12 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 import './FinNivel.css'
 
 const FinNivel = ({resultado}) => {
+    const navigate = useNavigate();
+
     return resultado === "victoria" ? (
         <div className="finNivel">
             <h1 className='finNivel-titulo'>Victoria</h1>
             <div className='finNivel-botones'>
-                <button className='finNivel-botones-siguiente'>Siguiente nivel</button>
-                <button>Seleccionar nivel</button>
+                <button className='finNivel-botones-accion'>Siguiente nivel</button>
+                <button className='finNivel-botones-accion' onClick={() => navigate('/selector-niveles')}>Seleccionar nivel</button>
             </div>
         </div>
     ) : (
