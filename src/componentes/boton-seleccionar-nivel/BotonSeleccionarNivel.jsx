c2e708d67e67e8d8570bf5fc31e2c443e45e56de
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2024-02-12 16:34:27 -0500

    Update button styles and functionality in BotonSeleccionarNivel component

--- src/componentes/boton-seleccionar-nivel/BotonSeleccionarNivel.jsx
+++ src/componentes/boton-seleccionar-nivel/BotonSeleccionarNivel.jsx
@@ -5,7 +5,7 @@ import './BotonSeleccionarNivel.css'
 
 const BotonSeleccionarNivel = ({cantidad}) => {
     const navegar = useNavigate();
-    const [mundoMaximo, setMundoMaximo] = useState(0);
+    const [mundoMaximo, setMundoMaximo] = useState(1);
     
     const entrarANivel = (nivel) => {
         localStorage.setItem('nivel', nivel);
@@ -21,15 +21,42 @@ const BotonSeleccionarNivel = ({cantidad}) => {
         };
     
         return (
-            <button
-                className="boton-seleccionar-nivel-button"
-                key={index}
-                onClick={handleClick}
-                disabled={index >= mundoMaximo}
+          <div
+            key={index}
+            className="botonGeneral"
+          >
+            <svg
+              className="boton-seleccionar-nivel-svg"
+              width="80"
+              height="80"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{
+                display: index >= mundoMaximo ? "block" : "none",
+                cursor: index >= mundoMaximo ? "not-allowed" : "pointer",
+              }}
             >
-                {index + 1}
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+              <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+              <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+            </svg>
+            <button
+              className="boton-seleccionar-nivel-button"
+              onClick={handleClick}
+              disabled={index >= mundoMaximo}
+              style={{
+                cursor: index >= mundoMaximo ? "not-allowed" : "pointer",
+              }}
+            >
+              {index >= mundoMaximo ? "" : index + 1}
             </button>
-            );
+          </div>
+        );
     });
     
     const filas = [];
