daf30b2be42d9c9b53df03146a8a1f223c8c4ce8
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-03-10 20:04:35 -0500

    Configurada la conexión del frontend con la base de datos.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:5071/api/v1',
+    baseURL: 'https://chafamon-web-api.azurewebsites.net/api/v1',
     headers: { 'Content-type': 'application/json' }
 })
